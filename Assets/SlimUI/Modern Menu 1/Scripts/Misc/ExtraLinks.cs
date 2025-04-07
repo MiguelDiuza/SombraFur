71a6d0b9c382d2e0293bd73f42d6ba9bf9aa295d
MiguelDiuza <132395122+MiguelDiuza@users.noreply.github.com>
2025-04-07 13:56:37 -0500

    menuInicioEnd
    f

---
+++ Assets/SlimUI/Modern Menu 1/Scripts/Misc/ExtraLinks.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SlimUI.ModernMenu{
+    public class ExtraLinks : MonoBehaviour{
+        public void CCP(){
+            Application.OpenURL("http://u3d.as/1JZG");
+        }
+
+        public void SciFi(){
+            Application.OpenURL("https://juancarmonal2004.wixsite.com/sombra-furtiva");
+        }
+
+        public void Clean1(){
+            Application.OpenURL("http://u3d.as/1hTi");
+        }
+
+        public void Essence(){
+            Application.OpenURL("http://u3d.as/1t11");
+        }
+    }
+}
